f9aa21ae568a324b60bc9baccc1d2360f350278c
Pat Canny <pat.canny@gmail.com>
2023-04-26 09:19:24 -0400

    R2023a version

--- tests/runFormalEquivTests.m
+++ tests/runFormalEquivTests.m
@@ -3,7 +3,7 @@ clear
 clc
 runTestsDialogResult = questdlg('Would you like to run project tests?','Run Project Tests','Yes','No','No');
 if strcmp(runTestsDialogResult,'Yes')
-    f = msgbox(["Tests will commence in 6 seconds.","Please ignore any windows that pop up until testing has completed.","Results will appear in the MATLAB Command Window."]);
+    f = msgbox(["Tests will commence momentarily and may take several minutes to complete.","Please ignore any windows that pop up until testing has completed.","Results will appear in the MATLAB Command Window."]);
     pause(6)
     close(f)
     clear f
